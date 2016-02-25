03069dd53c1da9b9f90d0ee323d0927ed973daa0
urbinao <oscar.urbina@telia.com>
2016-02-25 09:30:43 +0100

    Course and Activity Classes

---
+++ LMS/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class Activity
+    {
+        public int      Id          { get; set; }
+        public string   Name        { get; set; }
+        public DateTime StartTime   { get; set; }
+        public DateTime SlutTime    { get; set; }
+        public string   Description { get; set; }
+
+        public virtual ICollection<Course> Kurs { get; set; } // Activities are enrolled to one kurs.
+    }
+}
---
+++ LMS/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class Course
+    {
+        public int      Id          { get; set; }
+        public string   Name        { get; set; }
+        public DateTime StartTime   { get; set; }
+        public DateTime SlutTime    { get; set; }
+        public int      ActivityId  { get; set; }
+
+        //public virtual ICollection<Group> Group { get; set; } // Course are enrolled to one Group.
+    }
+}

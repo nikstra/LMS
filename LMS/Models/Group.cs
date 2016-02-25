404d1b256bfe8c8e1f3e7e50199897a563fe7979
sune <sunesjulafton@gmail.com>
2016-02-25 14:51:47 +0100

    Created Group views

--- LMS/Models/Group.cs
+++ LMS/Models/Group.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,18 @@ namespace LMS.Models
 {
     public class Group
     {
+
         public int Id { get; set; }
+        [Display(Name = "Namn")]
         public string Name { get; set; }
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Start datum")]
         public DateTime StartDate { get; set; }
+        [Display(Name = "Slut datum")]
         public DateTime EndDate { get; set; }
-        public int UserId { get; set; }
+
+        public int ApplicationUserId { get; set; }
 
         public virtual ICollection<ApplicationUser> Users { get; set; }
     }
--- LMS/Models/IdentityModels.cs
+++ LMS/Models/IdentityModels.cs
@@ -9,7 +9,8 @@ namespace LMS.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-
+        public int GroupId { get; set; }
+        
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -26,6 +27,8 @@ namespace LMS.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public DbSet<Group> Groups { get; set; }
 
         public static ApplicationDbContext Create()
         {

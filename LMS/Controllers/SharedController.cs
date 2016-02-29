160423e1b2ad2c39ec4dec6b340e6dc2f873b19e
Niklas Strand <nikstra@users.noreply.github.com>
2016-02-29 09:06:55 +0100

    Add shared controller and changed _LoginPartial.cshtml to use it.

---
+++ LMS/Controllers/SharedController.cs
@@ -0,0 +1,37 @@
+﻿using LMS.Models;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    /// <summary>
+    /// A shared controller. Can be used with partial views.
+    /// </summary>
+    public class SharedController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// Call this action in the view to get the group name.
+        /// @{Html.RenderAction("GroupName", "Shared");}
+        /// </summary>
+        /// <returns>
+        /// ContentResult.Content() with either the group name or
+        /// a string indicating that no group was found.
+        /// </returns>
+        public ActionResult GroupName()
+        {
+            db.Database.Log = (m => Debug.Print(m));
+
+            var activeUser = db.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .FirstOrDefault();
+
+            if (activeUser.Group != null)
+                return Content(activeUser.Group.Name);
+
+            return Content("Ingen grupp");
+        }
+    }
+}

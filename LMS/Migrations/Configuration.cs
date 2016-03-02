899e60737a7c01af38ba8ab513d47adb3313ae5d
Niklas Strand <nikstra@users.noreply.github.com>
2016-03-02 10:01:53 +0100

    Add basic seed and add FirstName/LastName to ApplicationUser.

--- LMS/Migrations/Configuration.cs
+++ LMS/Migrations/Configuration.cs
@@ -13,7 +13,7 @@ namespace LMS.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            //AutomaticMigrationDataLossAllowed = true;
             ContextKey = "LMS.Models.ApplicationDbContext";
         }
 
@@ -39,20 +39,73 @@ namespace LMS.Migrations
 
             //if (!context.Users.Any(u => u.UserName == "sune@gmail.com"))
             //{
-            //    //    var roleStore = new RoleStore<IdentityRole>(context);
-            //    //    var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
 
-            //    var store = new UserStore<ApplicationUser>(context);
-            //    var manager = new UserManager<ApplicationUser>(store);
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
             //    var user = new ApplicationUser { UserName = "sune@gmail.com" , GroupId = 4};
 
 
 
-            //    manager.Create(user, "password");
-            //    //    roleManager.Create(new IdentityRole { Name = "admin" });
-            //    //    manager.AddToRole(user.Id, "admin");
+            //manager.Create(user, "password");
+            string roleName = "teacher";
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new IdentityRole { Name = roleName });
+
+            roleName = "student";
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new IdentityRole { Name = roleName });
+
+            // Make sure to call ToList() to get a copy of users in memory, otherwise you'll get the following exception:
+            //      System.InvalidOperationException: There is already an open DataReader associated with this Command which must be closed first.
+            // Setting "MultipleActiveResultSets=true" in the connection string is not a proper solution for this issue.
+            // http://devproconnections.com/development/solving-net-scalability-problem
+            foreach (var user in context.Users.ToList())
+                userManager.AddToRole(user.Id, "student");
+
+            ApplicationUser newUser;
+            string userEmail = "principal.skinner@springfieldelementary.edu";
+            if (userManager.FindByEmail(userEmail) == null)
+            {
+                newUser = new ApplicationUser { UserName = "Seymore", Email = userEmail, FirstName = "Seymore", LastName = "Skinner" };
+                userManager.Create(newUser, "Abc_123");
+                userManager.AddToRole(newUser.Id, "teacher");
+            }
+
+            userEmail = "edna.krabappel@springfieldelementary.edu";
+            if (userManager.FindByEmail(userEmail) == null)
+            {
+                newUser = new ApplicationUser { UserName = "Edna", Email = userEmail, FirstName = "Edna", LastName = "Krabappel" };
+                userManager.Create(newUser, "Abc_123");
+                userManager.AddToRole(newUser.Id, "teacher");
+            }
+
+            userEmail = "superintendent.chalmers@springfieldelementary.edu";
+            if (userManager.FindByEmail(userEmail) == null)
+            {
+                newUser = new ApplicationUser { UserName = "Gary", Email = userEmail, FirstName = "Gary", LastName = "Chalmers" };
+                userManager.Create(newUser, "Abc_123");
+                userManager.AddToRole(newUser.Id, "teacher");
+            }
+
+            userEmail = "elizabeth.hoover@springfieldelementary.edu";
+            if (userManager.FindByEmail(userEmail) == null)
+            {
+                newUser = new ApplicationUser { UserName = "Elizabeth", Email = userEmail, FirstName = "Elizabeth", LastName = "Hoover" };
+                userManager.Create(newUser, "Abc_123");
+                userManager.AddToRole(newUser.Id, "teacher");
+            }
+
+            userEmail = "groundskeeper.willie@springfieldelementary.edu";
+            if (userManager.FindByEmail(userEmail) == null)
+            {
+                newUser = new ApplicationUser { UserName = "William", Email = userEmail, FirstName = "William", LastName = "MacDougal" };
+                userManager.Create(newUser, "Abc_123");
+                userManager.AddToRole(newUser.Id, "teacher");
+            }
             //}
-            
+
         }
     }
 }

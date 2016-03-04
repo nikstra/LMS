421fe4a56769eb36852ac39d84a80fbf87c95e30
Niklas Strand <nikstra@users.noreply.github.com>
2016-03-04 16:17:08 +0100

    Add start page for students.

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using LMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace LMS.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        /// <summary>
+        /// Application DB context
+        /// </summary>
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// User manager - attached to application DB context
+        /// </summary>
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public HomeController()
+        {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.ApplicationDbContext));
+        }
+
         public ActionResult Index()
         {
             // Temporarly using the about page as start page for authenticated users.
@@ -33,6 +53,33 @@ namespace LMS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Student()
+        {
+            var currentUser = UserManager.FindById(User.Identity.GetUserId());
+
+            if(currentUser.GroupId != null)
+            {
+                var usersInGroup = db.Users
+                    .Where(u => u.GroupId == currentUser.GroupId) ?? null;
+
+                TempData["ApplicationUsers"] = usersInGroup;
+            }
+            else
+            {
+                TempData["ApplicationUsers"] = new List<ApplicationUser>();
+            }
+
+            var upcomingActivities = db.Activities
+                .Where(a => a.StartDate >= DateTime.Now)
+                .Where(a => a.Course.GroupId == currentUser.GroupId)
+                .OrderBy(a => a.StartDate)
+                .Take(10)
+                .ToList();
+
+            return View(upcomingActivities);
+            //return View(db.Activities.ToList());
         }
     }
 }

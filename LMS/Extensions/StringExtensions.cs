d18b04a1a2b3506e1641d6794fe6cb7cffd0410e
Niklas Strand <nikstra@users.noreply.github.com>
2016-03-07 11:04:13 +0100

    Rewrite seed method and add extension methods.

---
+++ LMS/Extensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LMS.Extensions
+{
+    /// <summary>
+    /// Extension methods for System.String.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Extension method for String that removes diacritics (for example, accents and umlauts) from a string.
+        /// </summary>
+        /// <param name="selfString">The String object to be extended.</param>
+        /// <returns>Returns a string without any diacritics.</returns>
+        public static string RemoveDiacritics(this string selfString)
+        {
+            if (String.IsNullOrEmpty(selfString))
+                return String.Empty;
+
+            string inputFormD = selfString.Normalize(NormalizationForm.FormD);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < inputFormD.Length; i++)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(inputFormD[i]);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(inputFormD[i]);
+                }
+            }
+
+            return (stringBuilder.ToString().Normalize(NormalizationForm.FormC));
+        }
+    }
+}

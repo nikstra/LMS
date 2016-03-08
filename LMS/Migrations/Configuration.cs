8e4bb1b0ed9a94fdd19185da98e427f4e66b1ee3
Niklas Strand <nikstra@users.noreply.github.com>
2016-03-08 11:38:22 +0100

    Change menu item "Grupp" to "Kurser" and set column width in _ActivitiesCourse.cshtml.

--- LMS/Migrations/Configuration.cs
+++ LMS/Migrations/Configuration.cs
@@ -417,6 +417,14 @@ namespace LMS.Migrations
         //}
 
         #region SeedData
+
+        string[] loremIpsum = {
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            };
+
             //courses "C# grunder", "Grunderna i programmeringsspråket C#");
 
             //newActivity = AppendOrUpdateActivity(context, newCourse, "Föreläsning C#", "Föreläsning och codealong där vi lär oss grunderna i Visual Studio och C#", groupStartDate, 2);

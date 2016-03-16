b1b9b45fbe059c8816eec9a168302466b9bbbc91
Niklas Strand <nikstra@users.noreply.github.com>
2016-03-16 11:48:19 +0100

    Add javascript for date validation (not used yet).

---
+++ LMS/Scripts/DateValidation.js
@@ -0,0 +1,35 @@
+﻿// Include jquery in the html file.
+// <script src="@Url.Content("~/Scripts/jquery-1.10.2.min.js")" type="text/javascript"></script>
+$(document).ready(function () {
+
+    function validate(id)
+    {
+        var dt = $(id).val();
+
+        var d = new Date(dt);
+        var firstDate = new Date('@Model.Group.StartDate.ToString()');
+        var secondDate = new Date('@Model.Group.EndDate.ToString()');
+
+        $("#submitBtn").prop('disabled', false);
+
+        if (d.setHours(0, 0, 0, 0) < firstDate.setHours(0, 0, 0, 0)) {
+            $("#submitBtn").prop('disabled', true);
+            alert("Datumet får inte vara före gruppens startdatum: " + firstDate.toLocaleDateString('sv-SE'));
+        }
+        if (d.setHours(0, 0, 0, 0) > secondDate.setHours(0, 0, 0, 0)) {
+            $("#submitBtn").prop('disabled', true);
+            alert("Datumet får inte vara efter gruppens slutdatum: " + secondDate.toLocaleDateString('sv-SE'));
+        }
+
+    }
+
+    $("#StartDate").change(function () {
+
+        validate("#StartDate");
+    });
+
+    $("#EndDate").change(function () {
+
+        validate("#EndDate");
+    });
+});
